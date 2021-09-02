d1fa492727262c28a9271a107d32d2bbc60430c8
shabbir3g <mostafizshabbir@gmail.com>
2021-09-02 21:55:16 +0600

    almost final

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
+// Search book function 
 const searchBook = () => {
-
     const searchField = document.getElementById('search-field');
     const searchTitle = document.getElementById('search-title');
-
     
     const searchText = searchField.value;
     searchTitle.innerHTML = `Search results for: ${searchText}`;
     toggleSpinner('block');
-    booksToogleContainier('none');
+    booksToggleContainier('none');
     url = `https://openlibrary.org/search.json?q=${searchText}`;
 
     fetch(url)
@@ -17,49 +16,47 @@ const searchBook = () => {
     searchField.value = '';
 }
 
+// Notification Message Function 
 const notification = ( indendifier, content) => {
     const notification = document.getElementById('notification');
-    
     notification.innerHTML = `
         <div class="alert alert-${indendifier}">${content}</div>`
 }
 
+// Toggle spinner function 
 const toggleSpinner = displayStyle => {
-
     document.getElementById('spinner').style.display = displayStyle;
-
 }
 
-const booksToogleContainier = displayStyle => {
-
+// Toggle Books Container function 
+const booksToggleContainier = displayStyle => {
     document.getElementById('books-containier').style.display = displayStyle;
-
 }
 
+// Toggle Books Container function 
+const displaySearchResult = books => {
 
-const displaySearchResult = (books) => {
-
+    // Display Message Error handaling 
     if(books.length === 0){
-        notification('danger', `Couldn't find what you're looking for!`);
-        
-       
-    
-    }else{
-        notification('success', `${books.length} Books Found On Search Results`);
-      
+        notification('danger', `Sorry! No results found`);
     }
-    
-    
+    else{
+        notification('success', `${books.length} Books Found On Search Results`);
+    }
+
     const bookContainer = document.getElementById('display-books');
     bookContainer.textContent = "";
+
+    // Data forEach Loop
     books.forEach(book => {
-        
+
+        // Data adding to HTML
         const div = document.createElement('div');
-        bookImage = `https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : ''}-M.jpg`;
+        bookImage = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         div.classList.add('col');
         div.innerHTML = ` 
         <div class="card h-100">
-            <img src="${book.cover_i ? bookImage: 'images/book-placeholder.png'}" class="card-img-top" alt="...">
+            <img style="height: 436px; object-fit: contain;" src="${book.cover_i ? bookImage: 'images/book-placeholder.png'}" class="card-img-top" alt="Book image">
             <div class="card-body">
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text">
@@ -69,13 +66,11 @@ const displaySearchResult = (books) => {
             </p>
             </div>
         </div>`;
-        
+
         bookContainer.appendChild(div);
-        console.log(book);
     });
 
     toggleSpinner('none');
-    booksToogleContainier('block');
+    booksToggleContainier('block');
     
 }
-
